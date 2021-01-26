b60c4c3e57fc3374f894d91802365e5b57b41cfc
Dmitriy-Jakovlev <dimas39690023@gmail.com>
2021-01-26 19:06:30 +0200

    Update task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,7 @@
 const nameInput = document.querySelector('input');
 const nameOutput = document.querySelector('#name-output');
 
-const changeName = () => nameInput.value !== '' ? nameOutput.textContent = nameInput.value : nameOutput.textContent = 'незнакомец';
+// const changeName = () => nameInput.value !== '' ? nameOutput.textContent = nameInput.value : nameOutput.textContent = 'незнакомец';
+const changeName = () => nameOutput.textContent = !nameInput.value ? nameInput.value : 'незнакомец';
 
 nameInput.addEventListener('input', changeName);
